8fd67dbf0f8f003c1809067241369e35824c1822
Rachit Kakkad <kakkadrachit1@gmail.com>
2025-09-26 11:01:23 +0530

    Add solution: 611-valid-triangle-number

---
+++ 611-valid-triangle-number/2025-09-26 11.01.23 - Accepted - runtime 31ms - memory 8.3MB.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmpfunc(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int triangleNumber(int* nums, int numsSize) {
+    qsort(nums, numsSize, sizeof(int), cmpfunc);  
+    int count = 0;
+
+    for (int k = numsSize - 1; k >= 2; k--) {  
+        int i = 0, j = k - 1;
+        while (i < j) {
+            if (nums[i] + nums[j] > nums[k]) {
+                count += (j - i);  
+                j--;
+            } else {
+                i++;
+            }
+        }
+    }
+    return count;
+}
