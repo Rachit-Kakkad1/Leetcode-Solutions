32113db1ce9f18ec13546283bd91a2b14027cc37
Rachit Kakkad <kakkadrachit1@gmail.com>
2025-11-09 10:56:13 +0530

    Add solution: 2-add-two-numbers

---
+++ 2-add-two-numbers/2025-11-09 10.56.13 - Accepted - runtime 0ms - memory 12.5MB.c
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+        struct ListNode *head = NULL, *tail = NULL;
+    int carry = 0;
+
+    while (l1 != NULL || l2 != NULL || carry != 0) {
+        int x = (l1 != NULL) ? l1->val : 0;
+        int y = (l2 != NULL) ? l2->val : 0;
+        int sum = x + y + carry;
+
+        carry = sum / 10;
+        int digit = sum % 10;
+
+        struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+        newNode->val = digit;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+
+        if (l1 != NULL) l1 = l1->next;
+        if (l2 != NULL) l2 = l2->next;
+    }
+
+    return head;
+}
