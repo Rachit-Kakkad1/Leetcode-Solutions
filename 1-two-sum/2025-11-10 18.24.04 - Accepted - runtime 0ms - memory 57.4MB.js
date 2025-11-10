4e60f4c1068c06618721bc2867078c5979429267
Rachit Kakkad <kakkadrachit1@gmail.com>
2025-11-10 18:24:04 +0530

    Add solution: 1-two-sum

---
+++ 1-two-sum/2025-11-10 18.24.04 - Accepted - runtime 0ms - memory 57.4MB.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = Object.create(null);
+    for (let i = 0; i < nums.length; i++) {
+        if (map[target - nums[i]] !== undefined) return [map[target - nums[i]], i];
+        map[nums[i]] = i;
+    }
+};
