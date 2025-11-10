ca5d9d8f74405b1736b9c82d959792bee48c4cb8
Rachit Kakkad <kakkadrachit1@gmail.com>
2025-11-10 17:26:51 +0530

    Add solution: 224-basic-calculator

---
+++ 224-basic-calculator/2025-11-10 17.26.51 - Runtime Error - runtime NA - memory NA.c
@@ -0,0 +1,36 @@
+int calculate(char *s) {
+    int stack[10000];
+    int top = -1;
+    int res = 0, num = 0, sign = 1;
+
+    for (char *p = s; *p; p++) {
+        char c = *p;
+
+        if (c == ' ') continue;
+
+        if (c >= '0' && c <= '9') {
+            num = num * 10 + (c - '0');
+        } else {
+            if (c == '+' || c == '-') {
+                res += sign * num;
+                num = 0;
+                sign = (c == '+') ? 1 : -1;
+            } else if (c == '(') {
+                // Push sign first, then result for easy pop order
+                stack[++top] = res;
+                stack[++top] = sign;
+                res = 0;
+                sign = 1;
+            } else if (c == ')') {
+                res += sign * num;
+                num = 0;
+                int prevSign = stack[top--];
+                int prevRes = stack[top--];
+                res = prevRes + prevSign * res;
+            }
+        }
+    }
+
+    if (num) res += sign * num;
+    return res;
+}
