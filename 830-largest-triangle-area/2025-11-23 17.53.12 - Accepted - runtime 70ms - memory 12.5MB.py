de4936cd3290f5f57ef862dcf2b85c0bbd9e15d5
Rachit Kakkad <kakkadrachit1@gmail.com>
2025-11-23 17:53:12 +0530

    Add solution: 830-largest-triangle-area

---
+++ 830-largest-triangle-area/2025-11-23 17.53.12 - Accepted - runtime 70ms - memory 12.5MB.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def largestTriangleArea(self, points):
+        def area(a, b, c):
+            return abs(
+                a[0] * (b[1] - c[1]) +
+                b[0] * (c[1] - a[1]) +
+                c[0] * (a[1] - b[1])
+            ) / 2.0
+
+        n = len(points)
+        ans = 0.0
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                for k in range(j + 1, n):
+                    ans = max(ans, area(points[i], points[j], points[k]))
+
+        return ans
