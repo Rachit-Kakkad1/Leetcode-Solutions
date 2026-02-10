4926a9678a5540c6af4dd3aa62177c93de0db58b
Rachit Kakkad <kakkadrachit1@gmail.com>
2026-02-10 23:27:07 +0530

    Add solution: 136-single-number

---
+++ 136-single-number/2026-02-10 23.27.07 - Accepted - runtime 0ms - memory 56.7MB.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function singleNumber(nums) {
+    let result = 0;
+
+    for (const num of nums) {
+        result ^= num;
+    }
+
+    return result;
+}
