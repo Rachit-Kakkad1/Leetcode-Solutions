5d907db7588a9ea2c88182a755d726d73f5a0d6f
Rachit Kakkad <kakkadrachit1@gmail.com>
2026-02-10 23:03:36 +0530

    Add solution: 367-valid-perfect-square

---
+++ 367-valid-perfect-square/2026-02-10 23.03.36 - Accepted - runtime 0ms - memory 54.2MB.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+function isPerfectSquare(num) {
+    let left = 1;
+    let right = num;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        const square = mid * mid;
+
+        if (square === num) return true;
+        if (square < num) left = mid + 1;
+        else right = mid - 1;
+    }
+
+    return false;
+}
