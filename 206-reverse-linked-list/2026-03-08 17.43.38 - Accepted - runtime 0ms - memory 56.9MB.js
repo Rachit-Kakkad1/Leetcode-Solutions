907f69f225f4744387f0e0b3ab2460d54323b66a
Rachit Kakkad <kakkadrachit1@gmail.com>
2026-03-08 17:43:38 +0530

    Add solution: 206-reverse-linked-list

---
+++ 206-reverse-linked-list/2026-03-08 17.43.38 - Accepted - runtime 0ms - memory 56.9MB.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function ListNode(val, next = null) {
+    this.val = val;
+    this.next = next;
+}
+
+var reverseList = function(head) {
+    let prev = null;
+    let curr = head;
+
+    while (curr) {
+        let nextNode = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = nextNode;
+    }
+
+    return prev;
+};
