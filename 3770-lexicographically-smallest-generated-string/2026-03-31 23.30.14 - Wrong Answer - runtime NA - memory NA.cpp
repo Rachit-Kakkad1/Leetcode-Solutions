4b49434a701474416d807fc8761f0fe026921908
Rachit Kakkad <kakkadrachit1@gmail.com>
2026-03-31 23:30:14 +0530

    Add solution: 3770-lexicographically-smallest-generated-string

---
+++ 3770-lexicographically-smallest-generated-string/2026-03-31 23.30.14 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    string generateString(string str1, string str2) {
+        int n = str1.size(), m = str2.size();
+        string word(n + m - 1, '?');
+
+       
+        for (int i = 0; i < n; i++) {
+            if (str1[i] == 'T') {
+                for (int j = 0; j < m; j++) {
+                    if (word[i + j] == '?' || word[i + j] == str2[j]) {
+                        word[i + j] = str2[j];
+                    } else {
+                        return "";
+                    }
+                }
+            }
+        }
+
+      
+        for (int i = 0; i < word.size(); i++) {
+            if (word[i] == '?') {
+                for (char c = 'a'; c <= 'z'; c++) {
+                    word[i] = c;
+
+                    bool ok = true;
+
+              
+                    for (int start = max(0, i - m + 1); start <= min(i, n - 1); start++) {
+                        bool match = true;
+
+                        for (int j = 0; j < m; j++) {
+                            if (word[start + j] != str2[j]) {
+                                match = false;
+                                break;
+                            }
+                        }
+
+                        if (str1[start] == 'T' && !match) ok = false;
+                        if (str1[start] == 'F' && match) ok = false;
+
+                        if (!ok) break;
+                    }
+
+                    if (ok) break;
+                }
+
+                if (word[i] == '?') return "";
+            }
+        }
+
+        return word;
+    }
+};
