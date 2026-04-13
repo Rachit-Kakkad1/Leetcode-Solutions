b9e7bf44fd09944b2b56522764487e6d2b5bde6d
Rachit Kakkad <kakkadrachit1@gmail.com>
2026-04-13 10:08:49 +0530

    Added: To Be Or Not To Be (unknown)

---
+++ unknown/to-be-or-not-to-be/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {any} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(other) {
+            if (val === other) return true;
+            else throw new Error("Not Equal");
+        },
+        notToBe: function(other) {
+            if (val !== other) return true;
+            else throw new Error("Equal");
+        }
+    };
+};
